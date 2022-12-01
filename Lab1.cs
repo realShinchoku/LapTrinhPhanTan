09670e262d80d559c638391ab0d5368466e9d73a
Shinchoku <tienmp3@gmail.com>
2022-12-01 19:57:50 +0700

    Initial commit

---
+++ Lab1.cs
@@ -0,0 +1,39 @@
+﻿namespace console.Lab1
+{
+    internal class Lab1
+    {
+        private int[] _arr { get; set; }
+        private int _s { get; set; }
+        private int _e { get; set; }
+        private string _name { get; set; }
+        private int _count { get; set; } = 0;
+        private int _sum { get; set; } = 0;
+
+        public Lab1(int[] arr, int s, int e, string name)
+        {
+            _arr = arr;
+            _s = s;
+            _e = e;
+            _name = name;
+        }
+
+        public void Run()
+        {
+            for (int i = _s; i < _e; i++)
+            {
+                if (_arr[i] % 3 == 0)
+                {
+                    Console.WriteLine(_name + ": " + _arr[i]);
+                    _sum += _arr[i];
+                    _count++;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Sum " + _name + " = " + _sum);
+            Console.WriteLine("Count " + _name + " = " + _count);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using console.Lab1;
+
+Random random = new Random();
+int length = random.Next(1, 1000);
+int[] arr = new int[length];
+for (int i = 0; i < length; i++)
+{
+    arr[i] = random.Next(1, 100);
+}
+foreach (int i in arr) Console.Write(i.ToString() + " ");
+
+int k = random.Next(1, 10);
+int range = length / k, s = 0;
+Thread[] threads = new Thread[k];
+Lab1[] labs = new Lab1[k];
+
+for (int i = 1; i < k; i++)
+{
+    labs[i] = new Lab1(arr, s, s + range, i.ToString());
+    threads[i] = new Thread(labs[i].Run);
+    s += range;
+}
+foreach (var t in threads)
+{
+    t.Start();
+}
+foreach (var t in threads)
+{
+    try { t.Join(); } catch { }
+}
+foreach (var l in labs)
+{
+    l.Print();
+}
